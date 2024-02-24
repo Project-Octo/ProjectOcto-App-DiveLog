ddc0437c772a435bf929c5bff9860ac7c1dee0ca
HOAN_C <hoan.c9907@gmail.com>
2024-02-25 01:22:38 +0700

    Add animals img API connection

--- lib/screens/animals_page.dart
+++ lib/screens/animals_page.dart
@@ -10,10 +10,6 @@ class AnimalsPage extends StatelessWidget {
     'mackerel',
     'octopus',
     'turtle',
-    '강동헌',
-    '최정환',
-    'octopus',
-    'turtle'
   ];
 
   @override
@@ -58,12 +54,41 @@ class FishCard extends StatelessWidget {
               Expanded(
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(8),
-                  child: Image.asset(
-                    'assets/images/$fishName.png',
-                    height: 100.0,
-                    width: 100.0,
-                    fit: BoxFit.cover,
+                  child: FutureBuilder<String>(
+                    future: Future.delayed(
+                      const Duration(seconds: 1),
+                      () => _fetchImages(fishName),
+                    ),
+                    builder:
+                        (BuildContext context, AsyncSnapshot<String> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        // 로딩 화면 표시
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (snapshot.hasError) {
+                        // 오류 처리
+                        return Center(
+                          child: Text('Error: ${snapshot.error}'),
+                        );
+                      } else {
+                        // 이미지 표시
+                        return Image.network(
+                          snapshot.data ?? "", // 데이터 없을 때 빈 문자열 사용
+                          height: 100.0,
+                          width: 100.0,
+                          fit: BoxFit.cover,
+                        );
+                      }
+                    },
                   ),
+
+                  // child: Image.asset(
+                  //   'assets/images/$fishName.png',
+                  //   height: 100.0,
+                  //   width: 100.0,
+                  //   fit: BoxFit.cover,
+                  // ),
                 ),
               ),
               const SizedBox(height: 8.0),
@@ -133,13 +158,32 @@ class FishCard extends StatelessWidget {
                           ],
                         ),
                         const SizedBox(height: 8.0),
-                        Image.asset(
-                          'assets/images/$fishName.png',
-                          height: 200,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
+                        FutureBuilder<String>(
+                          future: _fetchImages(fishName),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<String> imageSnapshot) {
+                            if (imageSnapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              // 이미지를 불러오는 중일 때 로딩 화면을 표시합니다.
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            } else if (imageSnapshot.hasError) {
+                              // 오류가 발생한 경우 오류를 표시합니다.
+                              return Center(
+                                child: Text('Error: ${imageSnapshot.error}'),
+                              );
+                            } else {
+                              // 이미지를 정상적으로 불러왔을 때 이미지를 표시합니다.
+                              return Image.network(
+                                imageSnapshot.data ?? "",
+                                height: 200,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              );
+                            }
+                          },
                         ),
-                        const SizedBox(height: 8.0),
                         MarkdownBody(
                           styleSheet: MarkdownStyleSheet(
                             textScaleFactor: 1.1,
@@ -156,6 +200,46 @@ class FishCard extends StatelessWidget {
         );
       },
     );
+  }
+
+  //물고기 사진 API
+  Future<String> _fetchImages(String fishName) async {
+    final parameters = {
+      'q': fishName,
+      'engine': 'google_images',
+      'ijn': '0',
+      'api_key':
+          'cf7912d354898e1905347e40b7556b09240c1ed5b88eb41287d66c7b6e876fce'
+    };
+
+    final uri = Uri.https('serpapi.com', '/search', parameters);
+
+    print('###########################################');
+    print(uri);
+    print('###########################################');
+
+    final response = await http.get(uri);
+    print('###########################################');
+    print('await http get');
+    print('###########################################');
+
+    if (response.statusCode == 200) {
+      print('###########################################');
+      print('response.statusCode == 200');
+      print('###########################################');
+      final json = jsonDecode(response.body);
+      if (json.containsKey('images_results') &&
+          json['images_results'].isNotEmpty) {
+        // 이미지 링크 추출
+        final imageUrl = json['images_results'][0]['original'];
+        return imageUrl;
+      } else {
+        throw Exception('Image not found');
+      }
+    } else {
+      throw Exception(
+          'Failed to load image. Status code: ${response.statusCode}');
+    }
   }
 
   //물고기 정보 요청 API
